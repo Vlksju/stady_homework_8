feb027db4e35bef694bf0700e565dd44766c8426
Vlksju <xenya.vol2017@yandex.ru>
2022-10-06 13:32:07 +0500

    Solved task_60_1

--- task_60/Program.cs
+++ task_60/Program.cs
@@ -1,19 +1,63 @@
 ﻿Console.WriteLine("Программа, которая выводит элементы трехмерного массива вместе с их индексами");
 
-int[,,] FillRandomDoubleArray(int lines = 2, int columns = 2, int volume = 2, int leftRange = 0, int rethRange = 98)
+int[] FillRandomDoubleArray(int lines = 8, int leftRange = 0, int rethRange = 98)
 {
-    int[,,] randomArray = new int[lines, columns, volume];
+    int[] numbersRandom = new int[lines];
     Random rand = new Random();
-    for (int i = 0; i < randomArray.GetLength(0); i++)
+    for (int i = 0; i < numbersRandom.Length; i++)
     {
-        for (int j = 0; j < randomArray.GetLength(1); j++)
+        numbersRandom[i] = rand.Next(leftRange, rethRange + 1);
+    }
+    return numbersRandom;
+}
+
+int[] numbers = FillRandomDoubleArray();
+
+int maxNumber = numbers[0];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > maxNumber)
+    {
+        maxNumber = numbers[i];
+    }
+}
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    for (int j = i + 1; j < numbers.Length; j++)
+    {
+        if (numbers[i] == numbers[j])
         {
-            for (int k = 0; k < randomArray.GetLength(2); k++)
-                randomArray[i, j, k] = rand.Next(leftRange, rethRange + 1);
+            maxNumber++;
+            numbers[j] = maxNumber;
         }
     }
-    return randomArray;
 }
+
+
+int[,,] arrayOne = new int[2, 2, 2];
+int p = 0;
+
+for (int i = 0; i < arrayOne.GetLength(0); i++)
+{  
+    for (int j = 0; j < arrayOne.GetLength(1); j++)
+    {
+
+        for (int k = 0; k < arrayOne.GetLength(2); k++)
+        {
+
+
+            arrayOne[i, j, k] = numbers[p];
+            p++;
+          
+        }
+   
+    } 
+   
+}
+
+
 
 void PrintMatrix(int[,,] matrix)
 
@@ -30,8 +74,6 @@ void PrintMatrix(int[,,] matrix)
         Console.WriteLine();
     }
 }
-
-int[,,] arrayOne = FillRandomDoubleArray();
 
 Console.WriteLine("Массив: ");
 
